a5e270cd374d60b994ef1c84f7ae1dbe35f57813
Geobarteam <gvd8@hotmail.com>
2021-07-06 16:57:27 +0200

    integrate with Rabbitmq

--- SendRabbitMq/Program.cs
+++ SendRabbitMq/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Text;
+using System.Threading.Tasks;
 using RabbitMQ.Client;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.AppRole;
+using VaultSharp.V1.Commons;
+using VaultSharp.V1.SecretsEngines;
 
 namespace SendRabbitMq
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
-            var factory = new ConnectionFactory() { HostName = "localhost", Port = 32447, UserName = "token-01f10561-a457-c034-7b99-3ba0f0c77fab", Password ="i3BXNx73poqL5bQFcSlhv2aBLlaGtYBT7UAp" };
+            // use VaultSharp see doc: https://github.com/rajanadar/VaultSharp
+            // configure vault client
+            var vaultServerUriWithPort = "http://127.0.0.1:8200";
+            var roleid = "app-vaultrabbit-dev";
+            var secretid = "403f475c-7747-1eea-cb4f-98d2e3828ed8";
+            var vaultClientSettings = new VaultClientSettings(vaultServerUriWithPort, new AppRoleAuthMethodInfo(
+                roleid,
+                secretid
+            ));
+            var vaultClient = new VaultClient(vaultClientSettings);
+
+            // get dynamic secret from vault for rabbitmq
+            Secret<UsernamePasswordCredentials> secret = await vaultClient.V1.Secrets.RabbitMQ.GetCredentialsAsync("my-role");
+            string username = secret.Data.Username;
+            string password = secret.Data.Password;
+
+            // Connect to RabbitMQ with dynamic secret
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 32447, UserName = username, Password = password };
             using(var connection = factory.CreateConnection())
             using(var channel = connection.CreateModel())
             {
--- aks/scripts.ps1
+++ aks/scripts.ps1
@@ -16,25 +16,29 @@ vault secrets enable -path='secrets' -version=2 kv
 
 #Setup rabbitmq engine
 vault secrets enable rabbitmq
+#with path
 vault secrets enable -path='projects-api/rabbitmq' rabbitmq
+#configure connection settings
 vault write rabbitmq/config/connection \
     connection_uri="http://rabbitmq:15672" \
     username="guest" \
     password="guest" 
-
+#configure lease
 vault write rabbitmq/config/lease \
     ttl=60 \
     max_ttl=360000 
 
-# create my-role
+# create my-role that provide policy in RabbitMQ 
 vault write rabbitmq/roles/my-role \
-    vhosts='{"/":{"configure": ".*", "write": ".*", "read": ".*"}}' \
-    token_ttl=1h \
-	token_max_ttl=2h 
+    vhosts='{"/":{"configure": ".*", "write": ".*", "read": ".*"}}' 
 
-# get a token for RabbitMQ
+# get a token for RabbitMQ with my-role
 vault read rabbitmq/creds/my-role
 
+# add rabbitmq policy
+vault policy write rabbitmq ../rabbitmq-role-policy.hcl 
+
+#---Userpass
 # enable userpass auth method
 vault auth enable userpass
 
@@ -42,10 +46,19 @@ vault auth enable userpass
 vault write auth/userpass/users/app-vaultrabbit-dev password=starwars
 vault login -method=userpass username=app-vaultrabbit-dev
 
-# add rabbitmq policy
-vault policy write rabbitmq ../rabbitmq-role-policy.hcl 
-
+# add policy to userpass token
 vault write auth/userpass/users/app-vaultrabbit-dev \
     token_policies="rabbitmq" \
     token_ttl=1h \
-	token_max_ttl=2h 
+	token_max_ttl=2h
+
+#---Approle
+# create approle for app-vaultrabbit-dev
+vault write auth/approle/role/app-vaultrabbit-dev \
+	  role_id="app-vaultrabbit-dev" \
+		token_policies="rabbitmq" \
+		token_ttl=1h \
+		token_max_ttl=2h \
+		secret_id_num_uses=5
+# get secret-id
+vault write -f auth/approle/role/app-vaultrabbit-dev/secret-id
