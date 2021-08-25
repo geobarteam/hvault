ee6cdcc94fa6f185491d3fa9baace42895cdfbca
Geobarteam <gvd8@hotmail.com>
2021-08-25 15:15:30 +0200

    configure rabbitmq through scripts

--- Vms/script.ps1
+++ Vms/script.ps1
@@ -23,7 +23,7 @@ consul members
 vault server -config C:\Projects\hvault\Vms\vault\flxsrvpoc01.hcl -log-level=trace
 
 #set vault addrr
-$env:VAULT_ADDR="http://127.0.0.1:8200"
+$env:VAULT_ADDR = "http://127.0.0.1:8200"
 vault operator init
 Vault operator unseal #seal key here repeat 3x
 
@@ -33,18 +33,23 @@ vault operator generate-root
 vault operator generate-root -decode=K2ZNBgZrQ1BCKDEtMFk1OFxRXlkoIQVqAwY -otp=XH5l18q9rdEzfiyp162ilgP8aj
 
 #set vault token
-$env:VAULT_TOKEN='s.xj7S2i0LtWV0LHmgl0DFURbl'
+$env:VAULT_TOKEN = 's.xj7S2i0LtWV0LHmgl0DFURbl'
 
 #enable app role auth
 vault auth enable approle
 
 #Setup rabbitmq engine
-vault secrets enable rabbitmq
+vault secrets enable -path='[TenantName]/rabbitmq' rabbitmq
+
+##configure connection settings
+vault write rabbitmq/config/connection connection_uri="http://flxinflab01:15672" username="administrator" password="St@rwars3"
 
 #create a role for rabbitmq
+#configure lease
+vault write rabbitmq/config/lease ttl=60 max_ttl=360000 
+
 # create flbe-application that provide policy in RabbitMQ 
-vault write rabbitmq/roles/flbe-application \
-    vhosts='{"/":{"configure": ".*", "write": ".*", "read": ".*"}}'
+vault write rabbitmq/roles/flbe-application vhosts='{\"/\":{\"configure\": \".*\", \"write\": \".*\", \"read\": \".*\"}}'
 
 
 
